1f543ee1f490292ff18bd575de3addf9bfcb3d54
Pinkolik <mrpinkolik@gmail.com>
2022-12-18 01:59:05 +0500

    add fps logging

--- src/engine/intersection/IntersectionUtil.cpp
+++ src/engine/intersection/IntersectionUtil.cpp
@@ -1,26 +1,19 @@
 #include "IntersectionUtil.h"
 #include "glm/geometric.hpp"
 #include "Projection.h"
-#include <cmath>
 #include <glm/glm.hpp>
 #include <vector>
 
-glm::vec3 *IntersectionUtil::getMinimumTranslationVec(
-        std::vector<glm::vec3> &firstTriangle,
-        glm::vec3 &firstTriangleNormal,
-        std::vector<glm::vec3> &secondTriangle,
-        glm::vec3 &secondTriangleNormal) {
+glm::vec3 *
+IntersectionUtil::getMinimumTranslationVec(std::vector<glm::vec3> &firstTriangle, glm::vec3 &firstTriangleNormal,
+                                           std::vector<glm::vec3> &secondTriangle, glm::vec3 &secondTriangleNormal) {
     std::vector<glm::vec3> separatingAxes;
-    std::vector<glm::vec3> firstAxes =
-            getSeparatingAxes(firstTriangle, firstTriangleNormal);
-    std::vector<glm::vec3> secondAxes =
-            getSeparatingAxes(secondTriangle, secondTriangleNormal);
+    std::vector<glm::vec3> firstAxes = getSeparatingAxes(firstTriangle, firstTriangleNormal);
+    std::vector<glm::vec3> secondAxes = getSeparatingAxes(secondTriangle, secondTriangleNormal);
     separatingAxes.push_back(firstTriangleNormal);
     separatingAxes.push_back(secondTriangleNormal);
-    separatingAxes.insert(separatingAxes.end(), firstAxes.begin(),
-                          firstAxes.end());
-    separatingAxes.insert(separatingAxes.end(), secondAxes.begin(),
-                          secondAxes.end());
+    separatingAxes.insert(separatingAxes.end(), firstAxes.begin(), firstAxes.end());
+    separatingAxes.insert(separatingAxes.end(), secondAxes.begin(), secondAxes.end());
     float trans;
     glm::vec3 mtvAxis;
     for (auto &axis: separatingAxes) {
@@ -38,9 +31,7 @@ glm::vec3 *IntersectionUtil::getMinimumTranslationVec(
     return new glm::vec3(mtvAxis * trans * 1.5f);
 }
 
-std::vector<glm::vec3>
-IntersectionUtil::getSeparatingAxes(std::vector<glm::vec3> &triangle,
-                                    glm::vec3 &normal) {
+std::vector<glm::vec3> IntersectionUtil::getSeparatingAxes(std::vector<glm::vec3> &triangle, glm::vec3 &normal) {
     glm::vec3 ab, bc, ca;
     ab = triangle[1] - triangle[0];
     bc = triangle[2] - triangle[1];
